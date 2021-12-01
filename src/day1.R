dbc4fa4be29eb9ac16092fe96d390d318246cc04
Ch Julian Villabona Arenas <cj.villabona@gmail.com>
2021-12-01 11:19:42 +0000

    day one--part2 completed

--- src/day1.R
+++ src/day1.R
@@ -1,8 +1,20 @@
 input <- read.csv("data/day1.txt", header = F)
 
+#part1
 sonarSweep<-0;i<-1
 while(i<nrow(input)){
         if(input[i+1,]>input[i,]) sonarSweep <- sonarSweep+1
         i<-i+1
 };rm(i)
+sonarSweep
+
+#part2
+sonarSweep<-0
+codons <- c(1,2,3)
+while(codons[3]<nrow(input)){
+        x <- input[codons[1],] + input[codons[2],] + input[codons[3],]
+        codons <- codons+1
+        y <- input[codons[1],] + input[codons[2],] + input[codons[3],]
+        if(y>x) sonarSweep <- sonarSweep+1
+};rm(x,ycodons)
 sonarSweep
